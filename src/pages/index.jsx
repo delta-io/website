3c18906cc2c29328fe59cfcbfd61fd0b3b6b003c
Jake Bellacera <hi@jakebellacera.com>
2021-07-26 14:25:41 -0700

    Add eslint + prettier

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,10 @@
-import * as React from 'react';
+import * as React from "react";
 
 // styles
 const pageStyles = {
-  color: '#232129',
+  color: "#232129",
   padding: 96,
-  fontFamily: '-apple-system, Roboto, sans-serif, serif',
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
 };
 const headingStyles = {
   marginTop: 0,
@@ -12,16 +12,16 @@ const headingStyles = {
   maxWidth: 320,
 };
 const headingAccentStyles = {
-  color: '#663399',
+  color: "#663399",
 };
 const paragraphStyles = {
   marginBottom: 48,
 };
 const codeStyles = {
-  color: '#8A6534',
+  color: "#8A6534",
   padding: 4,
-  backgroundColor: '#FFF4DB',
-  fontSize: '1.25rem',
+  backgroundColor: "#FFF4DB",
+  fontSize: "1.25rem",
   borderRadius: 4,
 };
 const listStyles = {
@@ -36,20 +36,20 @@ const listItemStyles = {
 };
 
 const linkStyle = {
-  color: '#8954A8',
-  fontWeight: 'bold',
+  color: "#8954A8",
+  fontWeight: "bold",
   fontSize: 16,
-  verticalAlign: '5%',
+  verticalAlign: "5%",
 };
 
 const docLinkStyle = {
   ...linkStyle,
-  listStyleType: 'none',
+  listStyleType: "none",
   marginBottom: 24,
 };
 
 const descriptionStyle = {
-  color: '#232129',
+  color: "#232129",
   fontSize: 14,
   marginTop: 10,
   marginBottom: 0,
@@ -57,22 +57,22 @@ const descriptionStyle = {
 };
 
 const docLink = {
-  text: 'Documentation',
-  url: 'https://www.gatsbyjs.com/docs/',
-  color: '#8954A8',
+  text: "Documentation",
+  url: "https://www.gatsbyjs.com/docs/",
+  color: "#8954A8",
 };
 
 const badgeStyle = {
-  color: '#fff',
-  backgroundColor: '#088413',
-  border: '1px solid #088413',
+  color: "#fff",
+  backgroundColor: "#088413",
+  border: "1px solid #088413",
   fontSize: 11,
-  fontWeight: 'bold',
+  fontWeight: "bold",
   letterSpacing: 1,
   borderRadius: 4,
-  padding: '4px 6px',
-  display: 'inline-block',
-  position: 'relative',
+  padding: "4px 6px",
+  display: "inline-block",
+  position: "relative",
   top: -2,
   marginLeft: 10,
   lineHeight: 1,
@@ -81,47 +81,47 @@ const badgeStyle = {
 // data
 const links = [
   {
-    text: 'Tutorial',
-    url: 'https://www.gatsbyjs.com/docs/tutorial/',
+    text: "Tutorial",
+    url: "https://www.gatsbyjs.com/docs/tutorial/",
     description:
       "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: '#E95800',
+    color: "#E95800",
   },
   {
-    text: 'How to Guides',
-    url: 'https://www.gatsbyjs.com/docs/how-to/',
+    text: "How to Guides",
+    url: "https://www.gatsbyjs.com/docs/how-to/",
     description:
       "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: '#1099A8',
+    color: "#1099A8",
   },
   {
-    text: 'Reference Guides',
-    url: 'https://www.gatsbyjs.com/docs/reference/',
+    text: "Reference Guides",
+    url: "https://www.gatsbyjs.com/docs/reference/",
     description:
       "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: '#BC027F',
+    color: "#BC027F",
   },
   {
-    text: 'Conceptual Guides',
-    url: 'https://www.gatsbyjs.com/docs/conceptual/',
+    text: "Conceptual Guides",
+    url: "https://www.gatsbyjs.com/docs/conceptual/",
     description:
-      'Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.',
-    color: '#0D96F2',
+      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
+    color: "#0D96F2",
   },
   {
-    text: 'Plugin Library',
-    url: 'https://www.gatsbyjs.com/plugins',
+    text: "Plugin Library",
+    url: "https://www.gatsbyjs.com/plugins",
     description:
-      'Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.',
-    color: '#8EB814',
+      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
+    color: "#8EB814",
   },
   {
-    text: 'Build and Host',
-    url: 'https://www.gatsbyjs.com/cloud',
+    text: "Build and Host",
+    url: "https://www.gatsbyjs.com/cloud",
     badge: true,
     description:
-      'Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!',
-    color: '#663399',
+      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
+    color: "#663399",
   },
 ];
 
@@ -138,13 +138,8 @@ const IndexPage = () => (
       </span>
     </h1>
     <p style={paragraphStyles}>
-      Edit
-      {' '}
-      <code style={codeStyles}>src/pages/index.js</code>
-      {' '}
-      to see this page
-      update in real-time.
-      {' '}
+      Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
+      update in real-time.{" "}
       <span role="img" aria-label="Sunglasses smiley emoji">
         😎
       </span>
@@ -168,9 +163,9 @@ const IndexPage = () => (
               {link.text}
             </a>
             {link.badge && (
-            <span style={badgeStyle} aria-label="New Badge">
-              NEW!
-            </span>
+              <span style={badgeStyle} aria-label="New Badge">
+                NEW!
+              </span>
             )}
             <p style={descriptionStyle}>{link.description}</p>
           </span>
