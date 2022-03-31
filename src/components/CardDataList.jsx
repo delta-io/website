1a5c0ca7bafc8a174165efb6851436d7275a9cb1
Jake Bellacera <hi@jakebellacera.com>
2022-03-31 15:33:35 -0700

    Feature latest blog post (#54)
    * Pagination: add support for featured posts
    
    * Style the featured blog post
    
    Co-authored-by: Denny Lee <denny.g.lee@gmail.com>

--- src/components/CardDataList.jsx
+++ src/components/CardDataList.jsx
@@ -8,12 +8,57 @@ import {
   number,
   objectOf,
   object,
+  bool,
 } from "prop-types";
 import Link from "src/components/Link";
 import styled from "styled-components";
 import Grid from "src/components/Grid";
 import Typography, { TypographyContainer } from "src/components/Typography";
 import Embed from "src/components/Embed";
+
+const applyFeaturedColumnStyles = ({ spanColumns, theme }, styles) => {
+  if (spanColumns) {
+    const columnsMap = Object.entries(spanColumns).reduce(
+      (obj, [size, numColumns]) => ({
+        ...obj,
+        [size]: numColumns > 1 ? styles({ size, numColumns }) : undefined,
+      }),
+      {}
+    );
+
+    return theme.mediaBreakpointMap(columnsMap);
+  }
+
+  return "";
+};
+
+const Card = styled.div`
+  ${(props) => {
+    const { spanColumns, gutter, theme } = props;
+
+    return applyFeaturedColumnStyles(
+      { spanColumns, theme },
+      ({ numColumns, size }) => `
+        grid-column: span ${numColumns};
+        margin-bottom: ${theme.spacing.xl};
+        border: 1px solid ${theme.colors.border};
+        padding: ${numColumns > 2 ? theme.spacing.xl : theme.spacing.lg};
+
+        ${
+          theme.breakpointGreaterThan("md")(size) &&
+          `
+          display: grid;
+          grid-auto-flow: column;
+          grid-gap: ${theme.spacing[gutter]};
+          align-items: center;
+          grid-auto-columns: ${numColumns > 2 ? "4fr 6fr" : "1fr 1fr"};
+        `
+        }
+        
+      `
+    );
+  }}
+`;
 
 const ThumbnailContainer = styled.div`
   margin-bottom: ${(props) => props.theme.spacing.sm};
@@ -26,11 +71,81 @@ const ThumbnailContainer = styled.div`
   &:hover ~ ${TypographyContainer} a {
     text-decoration: underline;
   }
+
+  ${(props) => {
+    const { spanColumns, theme } = props;
+
+    return applyFeaturedColumnStyles({ spanColumns, theme }, ({ size }) => {
+      if (theme.breakpointGreaterThan("md")(size)) {
+        return `
+          min-height: 100%;
+          margin-bottom: 0;
+          display: flex;
+          align-items: center;
+
+          a {
+          margin-top: 0;
+          background-color: white;
+          }
+        `;
+      }
+
+      return `
+        margin-bottom: ${theme.spacing.lg};
+        max-width: 480px;
+      `;
+    });
+  }}
+`;
+
+const ContentContainer = styled(TypographyContainer)`
+  ${(props) => {
+    const { spanColumns, theme } = props;
+
+    return applyFeaturedColumnStyles(
+      { spanColumns, theme },
+      () => `
+        display: flex;
+        flex-flow: column;
+        justify-content: center;
+      `
+    );
+  }}
 `;
 
 const CardTitle = styled(Typography)`
   margin-top: 0;
   margin-bottom: ${(props) => props.theme.spacing.xs};
+
+  ${(props) => {
+    const { spanColumns, theme } = props;
+    const { fontSizes } = theme;
+
+    return applyFeaturedColumnStyles(
+      { spanColumns, theme },
+      ({ size }) =>
+        `
+        display: flex;
+        flex-flow: column;
+
+        &::before {
+          width: fit-content;
+          content: 'Latest post';
+          padding: .35em .5em;
+          background-color: ${theme.colors.accent};
+          font-size: 12px;
+          font-weight: ${theme.fontWeightBold};
+          z-index: 2;
+          margin-bottom: ${theme.spacing.xs};
+        }
+
+        ${
+          theme.breakpointGreaterThan("md")(size) &&
+          `font-size: ${fontSizes.h3};`
+        }
+        `
+    );
+  }}
 `;
 
 const CardContent = styled(Typography)`
@@ -61,6 +176,7 @@ const CardDataList = (props) => {
     thumbnailRatio,
     maxWidth,
     clampDescriptionLines,
+    showFeatured,
   } = props;
 
   if (!cards.length) {
@@ -79,56 +195,67 @@ const CardDataList = (props) => {
 
   return (
     <Grid columns={columns} gutter={gutter}>
-      {cards.map((card) => (
-        <div key={card.url}>
-          <ThumbnailContainer>
-            <Link href={card.url}>
-              <Embed
-                src={card.thumbnail}
-                aspectRatio={thumbnailRatio}
-                maxWidth={maxWidth}
-              />
-            </Link>
-          </ThumbnailContainer>
-          <TypographyContainer>
-            <CardTitle variant="h4">
-              <Link href={card.url} muted>
-                {card.title}
+      {cards.map((card, i) => {
+        const isFeatured = showFeatured && i === 0;
+
+        return (
+          <Card
+            key={card.url}
+            spanColumns={isFeatured ? columns : undefined}
+            gutter={gutter}
+          >
+            <ThumbnailContainer spanColumns={isFeatured ? columns : undefined}>
+              <Link href={card.url}>
+                <Embed
+                  src={card.thumbnail}
+                  aspectRatio={thumbnailRatio}
+                  maxWidth={maxWidth}
+                />
               </Link>
-            </CardTitle>
-            {card.meta && (
-              <>
-                <CardContent variant="p2">{card.meta}</CardContent>
-                <Typography variant="hr" density="dense" />
-              </>
-            )}
-            <CardContent variant="p2" lineClamp={clampDescriptionLines}>
-              {card.docs && (
-                <Link href={card.docs} muted>
-                  {" "}
-                  docs{" "}
+            </ThumbnailContainer>
+            <ContentContainer spanColumns={isFeatured ? columns : undefined}>
+              <CardTitle
+                variant="h4"
+                spanColumns={isFeatured ? columns : undefined}
+              >
+                <Link href={card.url} muted>
+                  {card.title}
                 </Link>
+              </CardTitle>
+              {card.meta && (
+                <>
+                  <CardContent variant="p2">{card.meta}</CardContent>
+                  <Typography variant="hr" density="dense" />
+                </>
               )}
-              {card.docs && card.source_code && " | "}
-              {card.source_code && (
-                <Link href={card.source_code} muted>
-                  {" "}
-                  source code{" "}
-                </Link>
-              )}
-              {card.docs && card.source_code && <br />}
-              {card.tags?.length && (
-                <CardContent variant="p2">
-                  {card.tags.map((tag) => (
-                    <CardTag key={tag}>{tag}</CardTag>
-                  ))}
-                </CardContent>
-              )}
-              {card.description}
-            </CardContent>
-          </TypographyContainer>
-        </div>
-      ))}
+              <CardContent variant="p2" lineClamp={clampDescriptionLines}>
+                {card.docs && (
+                  <Link href={card.docs} muted>
+                    {" "}
+                    docs{" "}
+                  </Link>
+                )}
+                {card.docs && card.source_code && " | "}
+                {card.source_code && (
+                  <Link href={card.source_code} muted>
+                    {" "}
+                    source code{" "}
+                  </Link>
+                )}
+                {card.docs && card.source_code && <br />}
+                {card.tags?.length && (
+                  <CardContent variant="p2">
+                    {card.tags.map((tag) => (
+                      <CardTag key={tag}>{tag}</CardTag>
+                    ))}
+                  </CardContent>
+                )}
+                {card.description}
+              </CardContent>
+            </ContentContainer>
+          </Card>
+        );
+      })}
     </Grid>
   );
 };
@@ -137,6 +264,7 @@ CardDataList.defaultProps = {
   clampDescriptionLines: undefined,
   density: undefined,
   maxWidth: undefined,
+  showFeatured: false,
 };
 
 CardDataList.propTypes = {
@@ -163,6 +291,7 @@ CardDataList.propTypes = {
   density: oneOf(["dense", "relaxed"]),
   thumbnailRatio: arrayOf(number).isRequired,
   maxWidth: string,
+  showFeatured: bool,
 };
 
 export default CardDataList;
