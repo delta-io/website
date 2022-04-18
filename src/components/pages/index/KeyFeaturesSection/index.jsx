969e083037e7a7e8d76b0c81f98d10f3145c599c
John ODwyer <johnpodwyer@gmail.com>
2022-04-18 13:01:52 -0600

    WIP - Updated key features to be linkable
    Signed-off-by: John ODwyer <johnpodwyer@gmail.com>

--- src/components/pages/index/KeyFeaturesSection/index.jsx
+++ src/components/pages/index/KeyFeaturesSection/index.jsx
@@ -1,20 +1,7 @@
 /* eslint-disable react/no-danger */
 import * as React from "react";
-import Grid from "src/components/Grid";
 import Section from "src/components/Section";
-import { TypographyContainer } from "src/components/Typography";
-import styled from "styled-components";
-import keyFeaturesImage from "./delta-lake-key-features.png";
-
-const KeyFeaturesSectionColumn = styled(TypographyContainer)`
-  text-align: center;
-
-  img {
-    width: 100%;
-    height: auto;
-    max-width: 1600px;
-  }
-`;
+import KeyFeaturesGrid from "src/components/pages/shared/KeyFeaturesGrid";
 
 const KeyFeaturesSection = () => (
   <Section
@@ -23,11 +10,65 @@ const KeyFeaturesSection = () => (
     centeredHeader
     padding="xxxl"
   >
-    <Grid columns={{ md: 1 }} gutter="xxl">
-      <KeyFeaturesSectionColumn>
-        <img src={keyFeaturesImage} alt="" />
-      </KeyFeaturesSectionColumn>
-    </Grid>
+    <KeyFeaturesGrid
+      features={[
+        {
+          image: "/images/featureIcons/acid_trans.png",
+          name: "ACID Transactions",
+          description:
+            "Protect your data with serializability, the strongest level of isolation",
+          link: "https://databricks.com/blog/2019/08/21/diving-into-delta-lake-unpacking-the-transaction-log.html",
+        },
+        {
+          image: "/images/featureIcons/scale_meta.png",
+          name: "Scalable Metadata",
+          description:
+            "Handle petabyte-scale tables with billions of partitions and files with ease",
+          link: "",
+        },
+        {
+          image: "/images/featureIcons/time_travel.png",
+          name: "Time Travel",
+          description:
+            "Access/revert to earlier versions of data for audits, rollbacks, or reproduce",
+          link: "https://databricks.com/blog/2019/02/04/introducing-delta-time-travel-for-large-scale-data-lakes.html",
+        },
+        {
+          image: "/images/featureIcons/open_source.png",
+          name: "Open Source",
+          description:
+            "Community driven, open standards, open protocol, open discussions",
+          link: "",
+        },
+        {
+          image: "/images/featureIcons/batch_stream.png",
+          name: "Unified Batch/Streaming",
+          description:
+            "Exactly once semantics ingestion to backfill to interactive queries",
+          link: "",
+        },
+        {
+          image: "/images/featureIcons/schem_evo.png",
+          name: "Schema Evolution / Enforcement",
+          description: "Prevent bad data from causing data corruption",
+          link: "https://databricks.com/blog/2019/09/24/diving-into-delta-lake-schema-enforcement-evolution.html",
+        },
+        {
+          image: "/images/featureIcons/audit_history.png",
+          name: "Audit History",
+          description:
+            "Delta Lake log all change details providing a fill audit trail",
+          link: "",
+        },
+        {
+          image: "/images/featureIcons/dml_ops.png",
+          name: "DML Operations",
+          description:
+            "SQL, Scala/Java and Python APIs to merge, update and delete datasets",
+          link: "",
+        },
+      ]}
+    />
   </Section>
 );
 
---
+++ src/components/pages/shared/KeyFeaturesGrid.jsx
@@ -0,0 +1,63 @@
+import * as React from "react";
+import { arrayOf, string } from "prop-types";
+import styled from "styled-components";
+import Link from "src/components/Link";
+import Grid from "src/components/Grid";
+import Typography from "src/components/Typography";
+
+const FeatureContainer = styled.div`
+  align-items: center;
+`;
+
+const Feature = styled(Link)`
+  display: block;
+  text-align: center;
+  overflow: hidden;
+`;
+
+const Avatar = styled.img`
+  max-width: 35%;
+  margin-left: auto;
+  margin-right: auto;
+  line-height: 0;
+  display: block;
+  margin-bottom: ${(props) => props.theme.spacing.sm};
+`;
+
+const FeatureNameContent = styled(Typography)`
+  color: ${(props) => props.theme.colors.primary};
+  font-size: ${(props) => props.theme.fontSizes.h4};
+  margin-top: 0;
+  margin-bottom: ${(props) => props.theme.spacing.xs};
+  line-height: 110%;
+`;
+
+const FeatureDescContent = styled(Typography)`
+  color: ${(props) => props.theme.colors.text};
+  margin-top: 0;
+  margin-bottom: ${(props) => props.theme.spacing.xs};
+`;
+
+const KeyFeaturesGrid = (props) => {
+  const { features } = props;
+
+  return (
+    <Grid columns={{ sm: 2, md: 3, lg: 4 }}>
+      {features.map((feature) => (
+        <FeatureContainer>
+          <Feature key={feature.name} href={`${feature.link}`} muted>
+            <Avatar src={feature.image} alt="" />
+            <FeatureNameContent>{feature.name}</FeatureNameContent>
+            <FeatureDescContent>{feature.description}</FeatureDescContent>
+          </Feature>
+        </FeatureContainer>
+      ))}
+    </Grid>
+  );
+};
+
+KeyFeaturesGrid.propTypes = {
+  features: arrayOf(string).isRequired,
+};
+
+export default KeyFeaturesGrid;
