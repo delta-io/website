4b4cd6178e55efdf8bd1350934fb8ab3a6e360f7
Vini Jaiswal <vini.js1303@gmail.com>
2022-05-03 11:50:37 -0700

    Delta 1.2 blog and Updates to main page highlights
    - Delta 1.2 blog released
    - Featured section updated with latest 4 community engagements.

--- src/components/pages/index/LatestUpdateSection/index.jsx
+++ src/components/pages/index/LatestUpdateSection/index.jsx
@@ -2,31 +2,32 @@ import * as React from "react";
 import ImageStrip from "src/components/ImageStrip";
 import Section from "src/components/Section";
 import styled from "styled-components";
-import thumbnail4 from "./extending-delta-sharing.png";
-import thumbnail3 from "./ama_2022-03-03.jpg";
-import thumbnail2 from "./ama_2022-02-17.jpg";
+import thumbnail4 from "./ama_2022-04-19.jpg";
+import thumbnail3 from "./delta-1-2-features.png";
+import thumbnail2 from "./ama_2022-04-28.jpg";
 import thumbnail1 from "./apache-flink-app-with-delta-lake.png";
 
 const updates = [
+  {
+    title:
+      "Delta Lake 1.2 - More Speed, Efficiency and Extensibility Than Ever",
+    thumbnail: thumbnail3,
+    url: "http://delta.io/blog/2022-05-05-delta-lake-1-2-released/",
+  },
+  {
+    title: "Community AMA (2022-04-28)",
+    thumbnail: thumbnail2,
+    url: "https://www.youtube.com/watch?v=oc1mlhtsyPg",
+  },
   {
     title: "Writing to Delta Lake from Apache Flink",
     thumbnail: thumbnail1,
     url: "http://delta.io/blog/2022-04-27-writing-to-delta-lake-from-apache-flink/",
   },
   {
-    title: "Extending Delta Sharing to Google Cloud Storage",
+    title: "Community AMA (2022-04-19)",
     thumbnail: thumbnail4,
-    url: "https://delta.io/blog/2022-03-11-delta-sharing-0-4-0-released/",
-  },
-  {
-    title: "Community AMA (2022-03-03)",
-    thumbnail: thumbnail3,
-    url: "https://www.youtube.com/watch?v=-KBbECH-oKQ",
-  },
-  {
-    title: "Community AMA (2022-02-17)",
-    thumbnail: thumbnail2,
-    url: "https://www.youtube.com/watch?v=oc1mlhtsyPg",
+    url: "https://www.youtube.com/watch?v=Iy2sb69iHGY",
   },
 ];
 
