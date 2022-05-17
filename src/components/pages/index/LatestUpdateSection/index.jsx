efca6a917da45bb4334f4c0428f382f27e3b2f7c
Denny Lee <denny.g.lee@gmail.com>
2022-05-16 19:09:56 -0700

    Add multi-cluster writes s3 blog

--- src/components/pages/index/LatestUpdateSection/index.jsx
+++ src/components/pages/index/LatestUpdateSection/index.jsx
@@ -2,12 +2,17 @@ import * as React from "react";
 import ImageStrip from "src/components/ImageStrip";
 import Section from "src/components/Section";
 import styled from "styled-components";
-import thumbnail4 from "./extending-delta-sharing.png";
+import thumbnail4 from "./S3-multi-cluster-writes-diagram.png";
 import thumbnail3 from "./delta-1-2-features.png";
 import thumbnail2 from "./ama_2022-04-28.jpg";
 import thumbnail1 from "./apache-flink-app-with-delta-lake.png";
 
 const updates = [
+  {
+    title: "Multi-cluster writes to Delta Lake Storage in S3",
+    thumbnail: thumbnail4,
+    url: "http://delta.io/blog/2022-05-18-multi-cluster-writes-to-delta-lake-storage-in-s3/",
+  },
   {
     title:
       "Delta Lake 1.2 - More Speed, Efficiency and Extensibility Than Ever",
@@ -23,11 +28,6 @@ const updates = [
     title: "Writing to Delta Lake from Apache Flink",
     thumbnail: thumbnail1,
     url: "http://delta.io/blog/2022-04-27-writing-to-delta-lake-from-apache-flink/",
-  },
-  {
-    title: "Extending Delta Sharing to Google Cloud Storage",
-    thumbnail: thumbnail4,
-    url: "https://delta.io/blog/2022-03-11-delta-sharing-0-4-0-released/",
   },
 ];
 
