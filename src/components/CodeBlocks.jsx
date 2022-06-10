5a70586e451e8cdf994ea689a558df8f71a2b046
Tathagata Das <tathagata.das1565@gmail.com>
2022-06-09 21:38:28 -0400

    Fixed utilities page

--- src/components/CodeBlocks.jsx
+++ src/components/CodeBlocks.jsx
@@ -2,20 +2,20 @@ import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import * as React from "react";
 
-const CBCreateTable = ({ sql, python, scala }) => {
-  return (
-    <Tabs>
-      <TabList>
-        {!sql || <Tab>SQL</Tab>}
-        {!python || <Tab>Python</Tab>}
-        {!scala || <Tab>Scala</Tab>}
-      </TabList>
+const CBCreateTable = ({ sql, python, scala, java }) => (
+  <Tabs>
+    <TabList>
+      {!sql || <Tab>SQL</Tab>}
+      {!python || <Tab>Python</Tab>}
+      {!scala || <Tab>Scala</Tab>}
+      {!java || <Tab>Java</Tab>}
+    </TabList>
 
-      {!sql || <TabPanel>{sql}</TabPanel>}
-      {!python || <TabPanel>{python}</TabPanel>}
-      {!scala || <TabPanel>{scala}</TabPanel>}
-    </Tabs>
-  );
-};
+    {!sql || <TabPanel>{sql}</TabPanel>}
+    {!python || <TabPanel>{python}</TabPanel>}
+    {!scala || <TabPanel>{scala}</TabPanel>}
+    {!java || <TabPanel>{java}</TabPanel>}
+  </Tabs>
+);
 
 export { CBCreateTable };
