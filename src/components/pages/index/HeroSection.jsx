4fba47ecf7fce40412acd390d1ecb3400b54160c
Denny Lee <denny.g.lee@gmail.com>
2022-08-02 15:57:17 -0700

    Update "releases" link

--- src/components/pages/index/HeroSection.jsx
+++ src/components/pages/index/HeroSection.jsx
@@ -72,7 +72,7 @@ const HeroSection = (props) => {
               Github
             </HeroLink>
             <HeroLink
-              href="https://github.com/delta-io/delta/releases/tag/v1.2.0"
+              href="https://github.com/delta-io/delta/releases/tag/v2.0.0"
               newTab
               muted
             >
