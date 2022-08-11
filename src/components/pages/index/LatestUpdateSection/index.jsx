7c6c8172b1fdda7ee5f32134141eb73d4555be3d
Denny Lee <denny.g.lee@gmail.com>
2022-08-11 09:33:24 -0700

    Including Apache Flink Source Blog (#122)
    * Include Apache Flink blog
    
    * Including author information
    
    * Updates per feedback

--- src/components/pages/index/LatestUpdateSection/index.jsx
+++ src/components/pages/index/LatestUpdateSection/index.jsx
@@ -3,15 +3,20 @@ import ImageStrip from "src/components/ImageStrip";
 import Section from "src/components/Section";
 import styled from "styled-components";
 import thumbnail4 from "./S3-multi-cluster-writes-diagram.png";
-import thumbnail3 from "./delta-1-2-features.png";
+import thumbnail3 from "./unified-source-interface.png";
 import thumbnail2 from "./ama_2022-06-09.jpg";
 import thumbnail1 from "./michael-armbrust-keynote.png";
 
 const updates = [
   {
+    title: "Apache Flink Source Connector for Delta Lake tables",
+    thumbnail: thumbnail3,
+    url: "/blog/2022-08-11-apache-flink-source-connector-for-delta-lake-tables/",
+  },
+  {
     title: "Delta Lake 2.0 - The Foundation of your Data Lakehouse is Open",
     thumbnail: thumbnail1,
-    url: "https://delta.io/blog/2022-08-02-delta-2-0-the-foundation-of-your-data-lake-is-open/",
+    url: "/blog/2022-08-02-delta-2-0-the-foundation-of-your-data-lake-is-open/",
   },
   {
     title: "Community AMA (2022-06-09)",
@@ -22,12 +27,6 @@ const updates = [
     title: "Multi-cluster writes to Delta Lake Storage in S3",
     thumbnail: thumbnail4,
     url: "http://delta.io/blog/2022-05-18-multi-cluster-writes-to-delta-lake-storage-in-s3/",
-  },
-  {
-    title:
-      "Delta Lake 1.2 - More Speed, Efficiency and Extensibility Than Ever",
-    thumbnail: thumbnail3,
-    url: "http://delta.io/blog/2022-05-05-delta-lake-1-2-released/",
   },
 ];
 
