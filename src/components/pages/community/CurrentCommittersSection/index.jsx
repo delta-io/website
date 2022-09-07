ae73f4d2a3bf3c97980e9027405e7392dff14c22
Jake Bellacera <hi@jakebellacera.com>
2022-09-06 22:54:34 -0700

    Make changes to community section

---
+++ src/components/pages/community/CurrentCommittersSection/index.jsx
@@ -0,0 +1,96 @@
+import * as React from "react";
+import Section from "src/components/Section";
+import Typography, { TypographyContainer } from "src/components/Typography";
+import Link from "src/components/Link";
+import Table from "src/components/Table";
+import styled from "styled-components";
+
+const committers = [
+  {
+    name: "R. Tyler Croy",
+    organization: "Scribd",
+    areas: ["delta", "delta-rs", "kafka-delta-ingest"],
+  },
+  {
+    name: "Tathagata Das",
+    organization: "Databricks",
+    areas: ["delta", "connectors"],
+  },
+  {
+    name: "QP Hou",
+    organization: "Neuralink",
+    areas: ["delta", "delta-rs"],
+  },
+  {
+    name: "Denny Lee",
+    organization: "Databricks",
+    areas: ["delta", "connectors", "delta-sharing", "delta-rs", "website"],
+  },
+  {
+    name: "Mykhailo Osypov",
+    organization: "Scribd",
+    areas: ["delta-rs", "kafka-delta-ingest"],
+  },
+  {
+    name: "Florian Valeye",
+    organization: "Backmarket",
+    areas: ["delta", "delta-rs"],
+  },
+  {
+    name: "Thomas Vollmer",
+    organization: "Microsoft",
+    areas: ["delta-rs"],
+  },
+  {
+    name: "Christian Williams",
+    organization: "Scribd",
+    areas: ["delta", "delta-rs", "kafka-delta-ingest"],
+  },
+  {
+    name: "Ryan Zhu",
+    organization: "Databricks",
+    areas: ["delta", "connectors", "delta-sharing"],
+  },
+];
+
+const CommittersTable = styled(Table)`
+  margin: 0 auto;
+`;
+
+const CurrentCommittersSection = () => (
+  <Section
+    background={(theme) => theme.light.bg}
+    title="Current Committers"
+    subtitle={
+      <TypographyContainer>
+        <Typography variant="p">
+          Want to dive deeper into Delta Lake, please chat with any of our
+          committers!
+        </Typography>
+        <Typography variant="p">
+          For more information on how to become a committer, please refer to{" "}
+          <Link href="https://github.com/delta-io/delta/blob/master/CONTRIBUTING.md">
+            Delta Lake committers
+          </Link>
+          .
+        </Typography>
+      </TypographyContainer>
+    }
+    centeredHeader
+    padding="xxxl"
+  >
+    <CommittersTable
+      headers={[
+        { label: "Name", field: "name" },
+        { label: "Organziation", field: "organization" },
+        { label: "Focal Areas", field: "areas" },
+      ]}
+      data={committers.map((committer) => ({
+        ...committer,
+        areas: committer.areas.join(", "),
+      }))}
+    />
+  </Section>
+);
+
+export default CurrentCommittersSection;
