bdaaf570b5640c84c14370d1b66ed6de037d13c8
Denny Lee <denny.g.lee@gmail.com>
2022-09-13 21:53:17 -0700

    TMo DSNA blog, update flink blog, update latest (#149)

--- src/components/pages/index/LatestUpdateSection/index.jsx
+++ src/components/pages/index/LatestUpdateSection/index.jsx
@@ -4,29 +4,36 @@ import Section from "src/components/Section";
 import styled from "styled-components";
 import thumbnail4 from "./d3l2-ryan_harris.png";
 import thumbnail3 from "./unified-source-interface.png";
-import thumbnail2 from "./community-office-hours_20220818.png";
-import thumbnail1 from "./michael-armbrust-keynote.png";
+import thumbnail2 from "./community-office-hours_2022-09-08.jpeg";
+import thumbnail1 from "./tmo-why-lakehouse-delta-lake.png";
+import thumbnail0 from "./generic-thumbnail.png";
 
 const updates = [
+  {
+    title:
+      "Why we migrated to a Data Lakehouse on Delta Lake for T-Mobile Data Science and Analytics Team",
+    thumbnail: thumbnail1,
+    url: "/blog/2022-09-14-why-migrate-lakehouse-delta-lake-tmo-dsna/",
+  },
   {
     title: "D3L2: Cybersecurity, Data Mesh, and Delta Lake at HSBC",
     thumbnail: thumbnail4,
     url: "https://youtu.be/ctKctx7vGW4",
   },
   {
-    title: "Community AMA (2022-08-18)",
+    title: "How to drop columns from a Delta Lake table",
+    thumbnail: thumbnail0,
+    url: "/blog/2022-08-29-delta-lake-drop-column/",
+  },
+  {
+    title: "Community Office Hours (2022-09-08)",
     thumbnail: thumbnail2,
-    url: "https://www.youtube.com/watch?v=lBr0UN7BEac&t=1s",
+    url: "https://youtu.be/LaKcKagdwHY",
   },
   {
     title: "Apache Flink Source Connector for Delta Lake tables",
     thumbnail: thumbnail3,
     url: "/blog/2022-08-11-apache-flink-source-connector-for-delta-lake-tables/",
-  },
-  {
-    title: "Delta Lake 2.0 - The Foundation of your Data Lakehouse is Open",
-    thumbnail: thumbnail1,
-    url: "/blog/2022-08-02-delta-2-0-the-foundation-of-your-data-lake-is-open/",
   },
 ];
 
