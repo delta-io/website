fcc425ec503c37115bb8a6dda24240b1d7cb217b
Denny Lee <denny.g.lee@gmail.com>
2022-09-16 08:39:49 -0700

    Update latest events (#152)

--- src/components/pages/community/LatestEventsSection/index.jsx
+++ src/components/pages/community/LatestEventsSection/index.jsx
@@ -5,39 +5,39 @@ import Grid from "src/components/Grid";
 import Section from "src/components/Section";
 import Typography, { TypographyContainer } from "src/components/Typography";
 import styled from "styled-components";
-import thumbnail4 from "./community-office-hours_20220818.png";
-import thumbnail3 from "./unified-source-interface.png";
-import thumbnail2 from "./michael-armbrust-keynote.png";
+import thumbnail4 from "./community-office-hours_2022-09-08.jpeg";
+import thumbnail3 from "./michael-armbrust-keynote.png";
+import thumbnail2 from "./AUA-thumbnail.jpeg";
 import thumbnail1 from "./d3l2-ryan_harris.png";
-import thumbnail0 from "./AUA-thumbnail.jpeg";
+import thumbnail0 from "./D3L2-TMobile-DSNA.png";
 
 const updates = [
+  {
+    title: "Simon and Denny Ask Us Anything - September 6, 2022",
+    thumbnail: thumbnail2,
+    url: "https://community.linuxfoundation.org/events/details/lfhq-delta-lake-presents-simon-denny-ask-us-anything-september-6-2022/",
+  },
+  {
+    title: "Delta Lake Community Office Hours (2022-09-08)",
+    thumbnail: thumbnail4,
+    url: "https://youtu.be/LaKcKagdwHY",
+  },
   {
     title: "D3L2: Cybersecurity, Data Mesh, and Delta Lake at HSBC",
     thumbnail: thumbnail1,
     url: "https://youtu.be/ctKctx7vGW4",
   },
   {
-    title: "Delta Lake Community Office Hours (2022-08-18)",
-    thumbnail: thumbnail4,
-    url: "https://www.youtube.com/watch?v=lBr0UN7BEac&t=1s",
-  },
-  {
-    title: "Apache Flink Source Connector for Delta Lake tables",
+    title: "Data + AI Summit 2022 | Michael Armbrust's Delta Lake Keynote",
     thumbnail: thumbnail3,
-    url: "/blog/2022-08-11-apache-flink-source-connector-for-delta-lake-tables/",
-  },
-  {
-    title: "Delta 2.0 - The Foundation of your Data Lakehouse is Open",
-    thumbnail: thumbnail2,
-    url: "/blog/2022-08-02-delta-2-0-the-foundation-of-your-data-lake-is-open/",
+    url: "https://youtu.be/O0Fr5X4i9os",
   },
 ];
 
 const featured = [
   {
     thumbnail: thumbnail0,
-    url: "https://community.linuxfoundation.org/events/details/lfhq-delta-lake-presents-simon-denny-ask-us-anything-september-6-2022/",
+    url: "https://community.linuxfoundation.org/events/details/lfhq-delta-lake-presents-d3l2-why-did-we-migrate-to-a-data-lakehouse-on-delta-lake-for-t-mobile-data-science-and-analytics-team/",
   },
 ];
 
@@ -77,21 +77,27 @@ const LatestEventsSection = () => (
       <FeaturedSectionColumn>
         <Typography variant="h3">
           <Link
-            href="https://community.linuxfoundation.org/events/details/lfhq-delta-lake-presents-d3l2-cybersecurity-data-mesh-and-delta-lake-at-hsbc/"
+            href="https://community.linuxfoundation.org/events/details/lfhq-delta-lake-presents-d3l2-why-did-we-migrate-to-a-data-lakehouse-on-delta-lake-for-t-mobile-data-science-and-analytics-team/"
             muted
           >
-            Simon and Denny Ask Us Anything - September 6, 2022
+            D3L2: Why did we migrate to a Data Lakehouse on Delta Lake for
+            T-Mobile Data Science and Analytics Team
           </Link>
         </Typography>
         <Typography variant="p">
-          Join us for the brand new monthly series &quot;Simon and Denny - Ask
-          Us Anything!&quot; where we will answer your data engineering
-          questions from building a data platform to ingestion to ETL to
-          analytics. With our background in SQL Server and BI to Apache Spark
-          and Delta Lake - we want to show you how to build your own lakehouse.
-          As this session is interactive, come prepared to ask questions all
-          throughout the session! Be prepared for another geeky, trans-Atlantic
-          event from two data nerds.
+          T-Mobile&#39;s mission to build the nation&#39;s best 5G network
+          drastically increased the number of monthly network projects planned
+          and directly impacted the enterprise procurement and supply chain
+          organizations. Like many enterprises, data at T-Mobile was spread
+          between disparate, unintegrated and complex systems.
+          <br />
+          In this session, we will discuss the how and why we migrated from
+          databases and data lakes to a data lakehouse on Delta Lake. Our
+          lakehouse architecture allows reading and writing of data without
+          blocking and scales out linearly. Business partners can easily adopt
+          advanced analytics and derive new insights. These new insights promote
+          innovation across disparate workstreams and solidify the decentralized
+          approach to analytics taken by T-Mobile.
         </Typography>
       </FeaturedSectionColumn>
     </Grid>
