592af0c020bc60e42b58b10510302a267e179d4c
VHromyk <108476400+dev-smart-ui@users.noreply.github.com>
2022-12-13 09:26:07 +0200

    added data to MDX provider

---
+++ src/components/pages/shared/YoutubeJsonCardDataList.jsx
@@ -0,0 +1,63 @@
+import * as React from "react";
+import { number, string } from "prop-types";
+import CardDataList from "src/components/CardDataList";
+import useDataList from "src/hooks/useDataList";
+
+const dataListProps = {
+  connectors: {
+    thumbnailRatio: [1, 1],
+    columns: { xs: 1, md: 2, lg: 3 },
+  },
+  videos: {
+    thumbnailRatio: [16, 9],
+    columns: { xs: 1, md: 2 },
+  },
+  videosYoutube: {
+    // thumbnailRatio: [16, 9],
+    columns: { xs: 1, md: 2 },
+  },
+  meetings: {
+    thumbnailRatio: [16, 9],
+    columns: { xs: 1, md: 2, lg: 3 },
+  },
+  tutorials: {
+    thumbnailRatio: [16, 9],
+    columns: { xs: 1, md: 2 },
+  },
+  tutorialsYoutube: {
+    // thumbnailRatio: [16, 9],
+    columns: { xs: 1, md: 2 },
+  },
+};
+
+const YoutubeJsonCardDataList = (props) => {
+  const { data: dataList, first } = props;
+  let cards = useDataList(dataList);
+
+  if (first) {
+    cards = cards.slice(0, first);
+  }
+
+  if (!cards) {
+    return null;
+  }
+
+  return (
+    <CardDataList
+      cards={cards}
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...dataListProps[dataList]}
+    />
+  );
+};
+
+YoutubeJsonCardDataList.defaultProps = {
+  first: undefined,
+};
+
+YoutubeJsonCardDataList.propTypes = {
+  data: string.isRequired,
+  first: number,
+};
+
+export default YoutubeJsonCardDataList;
