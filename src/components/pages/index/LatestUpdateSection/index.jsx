d113fe978fa78f78c8595dfe7a2053827089a22a
Nick Karpov <nick@nickkarpov.com>
2023-01-03 11:43:25 -0800

    Add Vacuum command post (#208)
    * adding delta vacuum command post
    * fix blog content
    * changes based on comments

--- src/components/pages/index/LatestUpdateSection/index.jsx
+++ src/components/pages/index/LatestUpdateSection/index.jsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import ImageStrip from "src/components/ImageStrip";
 import Section from "src/components/Section";
 import styled from "styled-components";
+
 import { graphql, useStaticQuery } from "gatsby";
 
 const CenteredImageStrip = styled(ImageStrip)`
