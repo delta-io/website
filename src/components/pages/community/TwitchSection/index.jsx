61d025dc504ba44e803515ff2ac27f03412b50f5
Denny Lee <denny.g.lee@gmail.com>
2023-01-05 21:27:14 -0800

    Include Twitch.tv live coding session

---
+++ src/components/pages/community/TwitchSection/index.jsx
@@ -0,0 +1,61 @@
+import * as React from "react";
+import Grid from "src/components/Grid";
+import Link from "src/components/Link";
+import Section from "src/components/Section";
+import Typography, { TypographyContainer } from "src/components/Typography";
+import styled from "styled-components";
+import twitchImg from "./twitch-logo-4931D91F85-seeklogo.com.png";
+import profileImg from "./Tyler-Croy-min.jpg";
+
+const ColumnWrapper = styled(TypographyContainer)`
+  text-align: left;
+  vertical-align: top;
+  margin: 0;
+  padding: 0;
+  img {
+    align: right;
+    text-align: right;
+    width: 120px;
+    height: auto;
+  }
+`;
+
+const NameWrapper = styled(TypographyContainer)`
+  font-size: 2rem;
+`;
+
+const TitleWrapper = styled(TypographyContainer)`
+  font-size: 1.5rem;
+`;
+
+const twitchSection = () => (
+  <Section background="white" padding="xl">
+    <center>
+      <Grid columns={{ xs: 1, sm: 1, md: 6 }} gutter="xl">
+        <ColumnWrapper>
+          <Link href="https://www.twitch.tv/agentdero">
+            <img src={profileImg} alt="R. Tyler Croy" />
+          </Link>
+        </ColumnWrapper>
+        <ColumnWrapper style={{ gridColumn: "span 4" }}>
+          <NameWrapper>
+            Watch Delta Rust code development LIVE on Twitch.tv!
+          </NameWrapper>
+          <TitleWrapper>
+            with{" "}
+            <Link href="https://www.twitch.tv/agentdero">
+              R. Tyler Croy (agentdero)
+            </Link>
+            , Delta Lake maintainer,
+            <br /> Director of Platform Engineering, Scribd
+          </TitleWrapper>
+        </ColumnWrapper>
+        <Link href="https://www.twitch.tv/agentdero">
+          <img src={twitchImg} alt="Twitch Logo" />
+        </Link>
+      </Grid>
+    </center>
+  </Section>
+);
+
+export default twitchSection;
