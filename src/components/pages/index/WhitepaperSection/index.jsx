f1860d7b98dabd633e2b8729e0f7a2eeb934a624
VHromyk <108476400+dev-smart-ui@users.noreply.github.com>
2023-04-04 12:57:11 +0300

    updated whitepapers section

--- src/components/pages/index/WhitepaperSection/index.jsx
+++ src/components/pages/index/WhitepaperSection/index.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Button from "src/components/Button";
 import Grid from "src/components/Grid";
+import Link from "src/components/Link";
 import Section from "src/components/Section";
 import Typography, { TypographyContainer } from "src/components/Typography";
 import styled from "styled-components";
@@ -13,7 +14,6 @@ const WhitepaperSectionColumn = styled(TypographyContainer)`
   img {
     width: 100%;
     height: auto;
-    max-width: 500px;
   }
 `;
 
@@ -21,18 +21,35 @@ const ButtonRow = styled(Typography)`
   margin-bottom: ${(props) => props.theme.spacing.xxl};
 `;
 
+const DocumentLink = styled(Link)`
+  text-decoration: none;
+  transition: 0.4s;
+
+  span {
+    display: block;
+    padding-bottom: 0.625rem;
+    margin: 0;
+    font-weight: 600;
+    color: ${(props) => props.theme.colors.text};
+  }
+
+  &:hover span,
+  &:focus span {
+    color: ${(props) => props.theme.colors.link};
+  }
+`;
+
 const WhitepaperSection = () => (
   <Section
     background={(theme) => theme.light.bg}
-    title="Read the Lakehouse Whitepaper"
+    title="Read the Lakehouse Storage Systems Whitepaper"
     subtitle={
       <>
         <Typography variant="p">
-          Together, the features of Delta Lake improve both the manageability
-          and performance of working with data in cloud storage objects, and
-          enable a “lakehouse” paradigm that combines the key features of data
-          warehouses and data lakes: standard DBMS management functions usable
-          against low-cost object stores.
+          This whitepaper dives into the features of Lakehouse storage systems
+          and compares Delta Lake, Apache Hudi, and Apache Iceberg. It explains
+          the benefits of Lakehouse storage systems and shows key performance
+          benchmarks.
         </Typography>
         <ButtonRow variant="p">
           <Button href="http://cidrdb.org/cidr2021/papers/cidr2021_paper17.pdf">
@@ -46,16 +63,20 @@ const WhitepaperSection = () => (
   >
     <Grid columns={{ md: 2 }} gutter="xxl">
       <WhitepaperSectionColumn>
-        <Typography variant="h5">
-          Pipeline using separate storage systems
-        </Typography>
-        <img src={separateStorage} alt="" />
+        <DocumentLink href="https://www.cidrdb.org/cidr2023/papers/p92-jain.pdf">
+          <Typography as="span">
+            Pipeline using separate storage systems
+          </Typography>
+          <img src={separateStorage} alt="" />
+        </DocumentLink>
       </WhitepaperSectionColumn>
       <WhitepaperSectionColumn>
-        <Typography variant="h5">
-          Using Delta Lake for both stream and table storage
-        </Typography>
-        <img src={streamTableStorage} alt="" />
+        <DocumentLink href="https://www.cidrdb.org/cidr2021/papers/cidr2021_paper17.pdf">
+          <Typography as="span">
+            Using Delta Lake for both stream and table storage
+          </Typography>
+          <img src={streamTableStorage} alt="" />
+        </DocumentLink>
       </WhitepaperSectionColumn>
     </Grid>
   </Section>
