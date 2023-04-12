450749f74b4debe14386c71659c6d25a37359a63
VHromyk <108476400+dev-smart-ui@users.noreply.github.com>
2023-04-12 13:14:19 +0300

    changed texts and reorganized the block

--- src/components/pages/index/WhitepaperSection/index.jsx
+++ src/components/pages/index/WhitepaperSection/index.jsx
@@ -5,11 +5,14 @@ import Link from "src/components/Link";
 import Section from "src/components/Section";
 import Typography, { TypographyContainer } from "src/components/Typography";
 import styled from "styled-components";
-import separateStorage from "./separate-storage.png";
+import separateStorage from "./separate-storage.jpg";
 import streamTableStorage from "./stream-table-storage.jpg";
 
 const WhitepaperSectionColumn = styled(TypographyContainer)`
   text-align: center;
+  background-color: white;
+  border-radius: 6px;
+  overflow: hidden;
 
   img {
     width: 100%;
@@ -25,6 +28,8 @@ const DocumentLink = styled(Link)`
   text-decoration: none;
   transition: 0.4s;
 
+  color: ${(props) => props.theme.colors.text};
+
   span {
     display: block;
     padding-bottom: 0.625rem;
@@ -33,23 +38,35 @@ const DocumentLink = styled(Link)`
     color: ${(props) => props.theme.colors.text};
   }
 
+  &:hover,
   &:hover span,
   &:focus span {
     color: ${(props) => props.theme.colors.link};
   }
 `;
 
+const DocumentTitle = styled.span`
+  display: inline-block;
+  min-height: 64px;
+`;
+
+const LinkWrapper = styled.div`
+  margin-top: 1rem;
+  display: flex;
+  justify-content: center;
+`;
+
 const WhitepaperSection = () => (
   <Section
     background={(theme) => theme.light.bg}
-    title="Read the Lakehouse Storage Systems Whitepaper"
+    title="Read the Lakehouse Storage Systems Whitepapers"
     subtitle={
       <>
         <Typography variant="p">
-          This whitepaper dives into the features of Lakehouse storage systems
-          and compares Delta Lake, Apache Hudi, and Apache Iceberg. It explains
-          the benefits of Lakehouse storage systems and shows key performance
-          benchmarks.
+          These whitepapers dive into the features of Lakehouse storage systems
+          and compare Delta Lake, Apache Hudi, and Apache Iceberg. They also
+          explain the benefits of Lakehouse storage systems and show key
+          performance benchmarks.
         </Typography>
         <ButtonRow variant="p">
           <Button href="http://cidrdb.org/cidr2021/papers/cidr2021_paper17.pdf">
@@ -64,21 +81,27 @@ const WhitepaperSection = () => (
     <Grid columns={{ md: 2 }} gutter="xxl">
       <WhitepaperSectionColumn>
         <DocumentLink href="https://www.cidrdb.org/cidr2023/papers/p92-jain.pdf">
-          <Typography as="span">
-            Pipeline using separate storage systems
-          </Typography>
+          <DocumentTitle>
+            Analyzing and Comparing Lakehouse Storage Systems
+          </DocumentTitle>
           <img src={separateStorage} alt="" />
         </DocumentLink>
       </WhitepaperSectionColumn>
       <WhitepaperSectionColumn>
         <DocumentLink href="https://www.cidrdb.org/cidr2021/papers/cidr2021_paper17.pdf">
-          <Typography as="span">
-            Using Delta Lake for both stream and table storage
-          </Typography>
+          <DocumentTitle>
+            Lakehouse: A New Generation of Open Platforms that Unify Data
+            Warehousing and Advanced Analytics
+          </DocumentTitle>
           <img src={streamTableStorage} alt="" />
         </DocumentLink>
       </WhitepaperSectionColumn>
     </Grid>
+    <LinkWrapper>
+      <DocumentLink href="https://dl.acm.org/doi/10.14778/3415478.3415560">
+        Delta lake: high-performance ACID table storage over cloud object stores
+      </DocumentLink>
+    </LinkWrapper>
   </Section>
 );
 
