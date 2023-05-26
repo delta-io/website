7de259c916a9d3d321416bd0e0f4e7526bfb8e0d
Jim Hibbard <8717652+JHibbard@users.noreply.github.com>
2023-05-26 13:15:19 -0700

    Update banner message to announce Delta Lake 2.4.0 release (#290)

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -25,7 +25,7 @@ const HomePage = () => {
   return (
     <PageLayout>
       <AnnouncementSection
-        title="Announcing Delta Lake 2.3.0 on Apache Spark™ 3.3"
+        title="Announcing Delta Lake 2.4.0 on Apache Spark™ 3.4"
         description="Try out the latest release today!"
         url="http://go.delta.io/latest"
       />
