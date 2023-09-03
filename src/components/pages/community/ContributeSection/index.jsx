0eeda8fb43d4f1523a34fd8fee26772ca2290ccc
manuteleco <mrg.foss@gmail.com>
2023-09-03 22:47:26 +0200

    Remove explicit references to the `connectors` repo (#325)
    The [connectors](https://github.com/delta-io/connectors) repository has
    been recently archived (on Jul 13, 2023). Its contents have been moved
    into the [delta](https://github.com/delta-io/delta) repository, under
    `connectors`.
    
    Therefore, the website should not include explicit mentions or calls for
    contributions to the archived `connectors` repository anymore.
    
    Co-authored-by: Denny Lee <denny.g.lee@gmail.com>

--- src/components/pages/community/ContributeSection/index.jsx
+++ src/components/pages/community/ContributeSection/index.jsx
@@ -49,23 +49,12 @@ const ContributeSection = () => (
           image: "/images/featureIcons/github_2048_black.png",
           name: "Delta",
           description:
-            "Delta Lake connector for Apache Spark™ with Scala/Java and Python APIs.",
+            "Delta Lake connectors for Apache Spark™, with Scala/Java and Python APIs, and for Java-based engines like Hive, Flink, PrestoDB, etc.",
           contributors: "192",
           orgs: "43",
           url: "https://github.com/delta-io/delta/",
           issue: "https://github.com/delta-io/delta/issues/new/choose",
           PR: "https://github.com/delta-io/delta/pulls",
-        },
-        {
-          image: "/images/featureIcons/github_2048_black.png",
-          name: "Delta Connectors",
-          description:
-            "Delta Lake connectors for non-Spark engines like Hive, Flink, Java, etc.",
-          contributors: "21",
-          orgs: "9",
-          url: "https://github.com/delta-io/connectors/",
-          issue: "https://github.com/delta-io/connectors/issues/new/choose",
-          PR: "https://github.com/delta-io/connectors/pulls",
         },
         {
           image: "/images/featureIcons/github_2048_black.png",
