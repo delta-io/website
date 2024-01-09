47b067a5cc47de04b993827a71668227c3ba35f4
Shangri <shangrialena@gmail.com>
2024-01-09 14:00:49 +0100

    fixed build errors

--- src/components/YoutubeCardDataList.jsx
+++ src/components/YoutubeCardDataList.jsx
@@ -186,7 +186,7 @@ const YoutubeCardDataList = ({ cards }) => {
   const [isEmbedId, setIsEmbedId] = useState(null);
   const navigationPrevRef = useRef(null);
   const navigationNextRef = useRef(null);
-
+  console.log(cards);
   const embedIdHandler = (embedId) => {
     setIsEmbedId(embedId);
     onOpen();
@@ -205,6 +205,7 @@ const YoutubeCardDataList = ({ cards }) => {
               {item.playlistDescription && (
                 <CollectionDescription>
                   {item.playlistDescription.replace(
+                    // eslint-disable-next-line no-misleading-character-class
                     /[\u{1F300}-\u{1F5FF}\u{1F600}-\u{1F64F}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FA6F}\u{1FA70}-\u{1FAFF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{1F1E0}-\u{1F1FF}\u{FE0F}]/gu,
                     ""
                   )}
@@ -240,6 +241,7 @@ const YoutubeCardDataList = ({ cards }) => {
                 {item?.videoCollection.map(
                   (slide, index) =>
                     slide.thumbnail && (
+                      // eslint-disable-next-line react/no-array-index-key
                       <SwiperSlide key={`${slide.id}-${index}`}>
                         <Card>
                           {item.playlistId === "manually_added_playlist_id" ? (
