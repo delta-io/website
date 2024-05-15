84f5869d30d26fd32b194957612415bc9b2101d7
Chris Abraham <cjyabraham@gmail.com>
2024-05-16 00:50:42 +0900

    Updates homepage announcement banner to 3.2.0 (#413)
    Signed-off-by: Chris Abraham <cjyabraham@gmail.com>

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -26,9 +26,9 @@ const HomePage = () => {
   return (
     <PageLayout>
       <AnnouncementSection
-        title="Announcing Delta Lake 3.1.0 on Apache Spark™ 3.5"
+        title="Announcing Delta Lake 3.2.0 on Apache Spark™ 3.5"
         description="Try out the latest release today!"
-        url="http://go.delta.io/latest"
+        url="https://github.com/delta-io/delta/releases/tag/v3.2.0"
       />
       <HeroSection
         title="Build Lakehouses with Delta Lake"
