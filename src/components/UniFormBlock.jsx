2c5a1d9755ba18a9945e3c580c83903e0d843853
Shangri <shangrialena@gmail.com>
2024-06-04 13:12:37 +0200

    changed UniForm Block

--- src/components/UniFormBlock.jsx
+++ src/components/UniFormBlock.jsx
@@ -4,10 +4,6 @@ import uniForm from "src/images/uniForm-img.png";
 import styled from "styled-components";
 import { media } from "config/theme";
 
-const Wrapper = styled.div`
-  padding: 2.5rem 0;
-`;
-
 const ImgItem = styled.a`
   width: 100%;
 
@@ -21,14 +17,12 @@ const ImgItem = styled.a`
 
 const UniFormBlock = () => (
   <Section title="UniForm" background="#f5f8f9" centeredHeader padding="xl">
-    <Wrapper>
-      <ImgItem
-        target="_blank"
-        href="https://docs.delta.io/latest/delta-uniform.html"
-      >
-        <img src={uniForm} alt="uniForm section" />
-      </ImgItem>
-    </Wrapper>
+    <ImgItem
+      target="_blank"
+      href="https://docs.delta.io/latest/delta-uniform.html"
+    >
+      <img src={uniForm} alt="uniForm section" />
+    </ImgItem>
   </Section>
 );
 
