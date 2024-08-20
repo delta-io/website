d16629df3eb908234e9a7b06d5c28251dc9d666f
Shangri <shangrialena@gmail.com>
2024-08-20 16:48:29 +0200

    added plausible-script

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,14 @@
-exports.onRenderBody = ({ setHtmlAttributes }) => {
+const React = require("react");
+
+exports.onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
   setHtmlAttributes({ lang: "en" });
+
+  setHeadComponents([
+    <script
+      key="plausible-script"
+      defer
+      data-domain="delta.io"
+      src="https://plausible.io/js/script.js"
+    ></script>,
+  ]);
 };
