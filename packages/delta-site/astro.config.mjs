de0dc1483db8500bf7392ca214565e9d04a5224a
Jake Bellacera <hi@jakebellacera.com>
2025-11-15 13:26:26 -0800

    Use astro-config
    - Used the astro-config package for managing theme configuration
    - Moved astro-favicon out of delta-theme
    - Moved Header and Footer components out of delta-theme
    - Moved pagefind css import out of Header/Search component
    
    Signed-off-by: Jake Bellacera <hi@jakebellacera.com>

--- packages/delta-site/astro.config.mjs
+++ packages/delta-site/astro.config.mjs
@@ -1,10 +1,12 @@
 import sitemap from "@astrojs/sitemap";
 import { defineConfig, envField } from "astro/config";
+import favicons from "astro-favicons";
 import { deltaTheme, remarkPlugins } from "delta-theme";
-
+import astroConfig from "astro-config";
 import netlify from "@astrojs/netlify";
 
-// https://astro.build/config
+const siteTitle = "Delta Lake";
+
 export default defineConfig({
   site: "https://delta.io/",
   scopedStyleStrategy: "where",
@@ -30,111 +32,121 @@ export default defineConfig({
   }),
   integrations: [
     sitemap(),
-    deltaTheme({
-      title: "Delta Lake",
-      menus: {
-        header: [
-          { label: "Sharing", url: "/sharing" },
-          { label: "Integrations", url: "/integrations" },
-          {
-            label: "Learn",
-            items: [
-              { label: "Getting Started", url: "/learn/getting-started" },
-              { label: "Blogs", url: "/blog" },
-              { label: "Tutorials", url: "/learn/tutorials" },
-              { label: "Videos", url: "/learn/videos" },
-              { label: "Case Studies", url: "/user-stories" },
-            ],
-          },
-          { label: "Roadmap", url: "/roadmap" },
-          { label: "Community", url: "/community" },
-          {
-            label: "Docs",
-            url: "https://docs.delta.io/",
-          },
-        ],
-        footerMain: [
-          {
-            url: "/sharing",
-            label: "Sharing",
-          },
-          {
-            url: "/integrations",
-            label: "Integrations",
-          },
-          {
-            url: "/roadmap",
-            label: "Roadmap",
-          },
-          {
-            url: "/blog",
-            label: "Blogs",
-          },
-        ],
-        footerLearn: [
-          {
-            label: "Getting Started",
-            url: "/learn/getting-started",
-          },
-          {
-            label: "Blogs",
-            url: "/blog",
-          },
-          {
-            label: "Tutorials",
-            url: "/learn/tutorials/",
-          },
-          {
-            label: "Videos",
-            url: "/learn/videos/",
-          },
-          {
-            label: "Case Studies",
-            url: "/user-stories",
-          },
-        ],
-        footerCommunity: [
-          {
-            url: "/community",
-            label: "Community",
-          },
-          {
-            url: "/resources/getting-help",
-            label: "Getting Help",
-          },
-          {
-            url: "/resources/contributing-to-delta",
-            label: "Contributing to Delta",
-          },
-        ],
-        social: [
-          {
-            label: "StackOverflow",
-            url: "https://stackoverflow.com/questions/tagged/delta-lake",
-            icon: "stackOverflow",
-          },
-          {
-            label: "GitHub",
-            url: "https://go.delta.io/github",
-            icon: "github",
-          },
-          {
-            label: "Twitter",
-            url: "https://go.delta.io/twitter",
-            icon: "twitter",
-          },
-          {
-            label: "Slack",
-            url: "https://go.delta.io/slack",
-            icon: "slack",
-          },
-          {
-            label: "LinkedIn",
-            url: "https://go.delta.io/linkedin",
-            icon: "linkedin",
-          },
-        ],
+    favicons({
+      name: siteTitle,
+      short_name: siteTitle,
+      background: "#042436",
+      themes: ["#00ADD4"],
+    }),
+    astroConfig({
+      name: "config",
+      config: {
+        title: siteTitle,
+        menus: {
+          header: [
+            { label: "Sharing", url: "/sharing" },
+            { label: "Integrations", url: "/integrations" },
+            {
+              label: "Learn",
+              items: [
+                { label: "Getting Started", url: "/learn/getting-started" },
+                { label: "Blogs", url: "/blog" },
+                { label: "Tutorials", url: "/learn/tutorials" },
+                { label: "Videos", url: "/learn/videos" },
+                { label: "Case Studies", url: "/user-stories" },
+              ],
+            },
+            { label: "Roadmap", url: "/roadmap" },
+            { label: "Community", url: "/community" },
+            {
+              label: "Docs",
+              url: "https://docs.delta.io/",
+            },
+          ],
+          footerMain: [
+            {
+              url: "/sharing",
+              label: "Sharing",
+            },
+            {
+              url: "/integrations",
+              label: "Integrations",
+            },
+            {
+              url: "/roadmap",
+              label: "Roadmap",
+            },
+            {
+              url: "/blog",
+              label: "Blogs",
+            },
+          ],
+          footerLearn: [
+            {
+              label: "Getting Started",
+              url: "/learn/getting-started",
+            },
+            {
+              label: "Blogs",
+              url: "/blog",
+            },
+            {
+              label: "Tutorials",
+              url: "/learn/tutorials/",
+            },
+            {
+              label: "Videos",
+              url: "/learn/videos/",
+            },
+            {
+              label: "Case Studies",
+              url: "/user-stories",
+            },
+          ],
+          footerCommunity: [
+            {
+              url: "/community",
+              label: "Community",
+            },
+            {
+              url: "/resources/getting-help",
+              label: "Getting Help",
+            },
+            {
+              url: "/resources/contributing-to-delta",
+              label: "Contributing to Delta",
+            },
+          ],
+          social: [
+            {
+              label: "StackOverflow",
+              url: "https://stackoverflow.com/questions/tagged/delta-lake",
+              icon: "stackOverflow",
+            },
+            {
+              label: "GitHub",
+              url: "https://go.delta.io/github",
+              icon: "github",
+            },
+            {
+              label: "Twitter",
+              url: "https://go.delta.io/twitter",
+              icon: "twitter",
+            },
+            {
+              label: "Slack",
+              url: "https://go.delta.io/slack",
+              icon: "slack",
+            },
+            {
+              label: "LinkedIn",
+              url: "https://go.delta.io/linkedin",
+              icon: "linkedin",
+            },
+          ],
+        },
       },
     }),
+    deltaTheme(),
   ],
 });
--- packages/delta-theme/src/deltaTheme.ts
+++ packages/delta-theme/src/deltaTheme.ts
@@ -1,20 +1,7 @@
-import favicons from "astro-favicons";
 import type { AstroIntegration } from "astro";
 import { themePlugin } from "./themePlugin";
-import type { ConfigOptions as ThemeConfig } from "./configSchema";
 import { searchPlugin } from "./searchPlugin";
 
-export const deltaTheme = (config: ThemeConfig): AstroIntegration[] => {
-  const { title } = config;
-
-  return [
-    themePlugin(config),
-    searchPlugin(),
-    favicons({
-      name: title,
-      short_name: title,
-      background: "#042436",
-      themes: ["#00ADD4"],
-    }),
-  ];
+export const deltaTheme = (): AstroIntegration[] => {
+  return [themePlugin(), searchPlugin()];
 };
